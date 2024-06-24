4ecbde5e40bbfb67484f50b0ede100cc2f416b5b
Grisha <grisha765@tuta.io>
2024-06-24 17:37:22 +0300

    [u] base adaptation

--- core/base.py
+++ core/base.py
@@ -1,11 +1,13 @@
 from core.browser import init_browser
 from scrap.start import prepare_page
+from scrap.login import login_hh
 
 async def main():
     browser, page, playwright = await init_browser()
     login = str(input("login: "))
     password = str(input("password: "))
-    await prepare_page(page, login, password)
+    await prepare_page(page)
+    await login_hh(page, login, password)
     await browser.close()
     await playwright.stop()
 
