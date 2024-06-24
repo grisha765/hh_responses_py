3b862a774e36677410a7a5fad98ce1be2b9f131e
Grisha <grisha765@tuta.io>
2024-06-24 17:02:19 +0300

    [u] entering login and password

--- scrap/start.py
+++ scrap/start.py
@@ -1,12 +1,28 @@
-import asyncio
-from core.screenshot import take_screenshot
+from config.config import Config
 from config import logging_config
 logging = logging_config.setup_logging(__name__)
 
-async def prepare_page(page):
+async def prepare_page(page, login: str = Config.hh_login, password: str = Config.hh_passw):
     logging.info("Navigating to hh.ru...")
-    await page.goto('https://spb.hh.ru/account/login')
-    await take_screenshot(page, "test.png")
+    await page.goto('https://spb.hh.ru/account/login', wait_until='domcontentloaded')
+    await page.wait_for_selector('button[data-qa="expand-login-by-password"]', timeout=60000)
+    logging.debug('Click the button "Войти с паролем"')
+    await page.click('button[data-qa="expand-login-by-password"]')
+    email_text = login
+    password_text = password
+    logging.debug(f'Write the text {email_text} in the login field.')
+    await page.fill('input[data-qa="login-input-username"]', email_text)
+    logging.debug(f'Write the text {password_text} in the password field.')
+    await page.fill('input[data-qa="login-input-password"]', password_text)
+    email_placeholder = await page.get_attribute('input[data-qa="login-input-username"]', 'placeholder')
+    email_value = await page.get_attribute('input[data-qa="login-input-username"]', 'value')
+    password_placeholder = await page.get_attribute('input[data-qa="login-input-password"]', 'placeholder')
+    password_value = await page.get_attribute('input[data-qa="login-input-password"]', 'value')
+
+    return {"Email Placeholder": f"{email_placeholder}",
+            "Email Value": f"{email_value}",
+            "Password Placeholder": "{password_placeholder}",
+            "Password Value": f"{password_value}"}
 
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")
