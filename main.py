aa420abc2dee69af438040e7d609c09e401799c7
Grisha <grisha765@tuta.io>
2024-06-24 15:54:34 +0300

    [+] add run in main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 from config.config import Config
 from config import logging_config
 logging = logging_config.setup_logging(__name__)
@@ -5,3 +6,7 @@ logging = logging_config.setup_logging(__name__)
 logging.info(f"Инициализация скрипта, уровень логгирования: {Config.log_level}")
 logging.debug(f"chrome_path: {Config.chrome_path}")
 logging.debug(f"tgbot_pass: {Config.tgbot_pass}")
+
+if __name__ == '__main__':
+    from core.base import main
+    asyncio.run(main())
