4808eb95185e4f37c112f42af11eb7db92f40f81
Grisha <grisha765@tuta.io>
2024-06-24 17:02:30 +0300

    [+] init tests

---
+++ tests/test_hh_login.py
@@ -0,0 +1,25 @@
+from core.browser import init_browser
+from scrap.start import prepare_page
+from config.config import Config
+from config import logging_config
+logging = logging_config.setup_logging(__name__)
+
+async def test_login():
+    browser, page, playwright = await init_browser()
+    expected_email_value = Config.hh_login
+    expected_password_value = Config.hh_passw
+    result = await prepare_page(page)
+    try:
+        assert result["Email Value"] == expected_email_value, f"Expected {expected_email_value}, but got {result['Email Value']}"
+    except AssertionError as e:
+        logging.error(f"Email Value test failed: {e}")
+    try:
+        assert result["Password Value"] == expected_password_value, f"Expected {expected_password_value}, but got {result['Password Value']}"
+    except AssertionError as e:
+        logging.error(f"Password Value test failed: {e}")
+    logging.info(f'Test passed!{"\x1b[0m"}')
+    await browser.close()
+    await playwright.stop()
+
+if __name__ == "__main__":
+    raise RuntimeError("This module should be run only via main.py")
