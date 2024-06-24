5ddcf0c601ab2fe780007fa20831128e247b0290
Grisha <grisha765@tuta.io>
2024-06-24 17:02:41 +0300

    [u] add run tests in main

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from config import logging_config
 logging = logging_config.setup_logging(__name__)
 
 logging.info(f"Инициализация скрипта, уровень логгирования: {Config.log_level}")
+logging.debug(f"tests: {Config.tests}")
 logging.debug(f"chrome_path: {Config.chrome_path}")
 logging.debug(f"tgbot_pass: {Config.tgbot_pass}")
 
 if __name__ == '__main__':
-    from core.base import main
-    asyncio.run(main())
+    if Config.tests == "True":
+        from tests.run import run
+        asyncio.run(run())
+    else:
+        from core.base import main
+        asyncio.run(main())
