ea993ace9ad780869d09f7eb4c95b1f965d8e62e
Grisha <grisha765@tuta.io>
2024-11-19 18:16:10 +0300

    [f] feh in background

--- core/screenshot.py
+++ core/screenshot.py
@@ -7,7 +7,7 @@ async def take_screenshot(page, file_name):
     logging.info(f"Taking screenshot: {file_name}")
     await page.screenshot(path=file_name)
     print(f"Screenshot saved as {file_name}")
-    os.system(f"feh {file_name}")
+    os.system(f"feh {file_name} &")
 
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")
