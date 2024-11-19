5266500f95d569fa699872cb51dffb8b00dad07e
Grisha <grisha765@tuta.io>
2024-11-19 18:16:36 +0300

    [r] refactor login

--- scrap/login.py
+++ scrap/login.py
@@ -1,52 +1,40 @@
 import asyncio
 from core.screenshot import take_screenshot
-from scrap.passwd import login_password
 from config.config import Config
 from config import logging_config
 logging = logging_config.setup_logging(__name__)
 
-async def login_hh(page, login: str = Config.hh_login, password: str = Config.hh_passw):
-    for attempt in range(3):
-        try:
-            await page.click('button[data-qa="expand-login-by-password"]')
-            await asyncio.sleep(2)
-            if not await page.is_visible('button[data-qa="expand-login-by-password"]'):
-                break
-        except Exception as e:
-            logging.warning(f'Attempt {attempt + 1} failed with error: {e}')
-            if attempt == 3 - 1:
-                raise e
-
-    email_text = login
-    password_text = password
+async def login_hh(page):
+    email_text = Config.hh_login
 
     logging.debug(f'Write the text {email_text} in the login field.')
     await asyncio.sleep(2)
-    try:
-        await page.fill('input[data-qa="login-input-username"]', email_text)
-    except Exception as e:
-        logging.error(f"Text login writing error: {e}")
-        await take_screenshot(page, "tests/error_login.png")
+    await page.fill('input[name="login"]', email_text)
 
-    logging.debug(f'Write the text {password_text} in the password field.')
+    await page.locator("button:has-text('Продолжить')").click()
+    await asyncio.sleep(3)
+
+    captcha_locator = page.locator('input[name="captchaText"]')
+
+    if await captcha_locator.is_visible():
+        logging.warning("Capcha is detect!")
+        await take_screenshot(page, "capcha.png")
+        capcha_text = input("Enter the text from the image: ")
+        await page.fill('input[name="captchaText"]', capcha_text)
+
+        await page.locator("button:has-text('Продолжить')").click()
+
+    await take_screenshot(page, "img.png")
+
+    otp_text = input("Email otp: ")
+
+    logging.debug(f'Write the text {otp_text} in the otp field.')
     await asyncio.sleep(2)
-    try:
-        await page.fill('input[data-qa="login-input-password"]', password_text)
-    except Exception as e:
-        logging.error(f"Text password writing error: {e}")
-        await take_screenshot(page, "tests/error_password.png")
+    await page.fill('input[name="otp-code-input"]', otp_text)
 
-    email_placeholder = await page.get_attribute('input[data-qa="login-input-username"]', 'placeholder')
-    email_value = await page.get_attribute('input[data-qa="login-input-username"]', 'value')
-    password_placeholder = await page.get_attribute('input[data-qa="login-input-password"]', 'placeholder')
-    password_value = await page.get_attribute('input[data-qa="login-input-password"]', 'value')
-
-    await login_password(page)
-
-    return {"Email Placeholder": f"{email_placeholder}",
-            "Email Value": f"{email_value}",
-            "Password Placeholder": f"{password_placeholder}",
-            "Password Value": f"{password_value}"}
+    await page.locator("button:has-text('Подтвердить')").click()
+    await asyncio.sleep(2)
+    await take_screenshot(page, "img.png")
 
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")
