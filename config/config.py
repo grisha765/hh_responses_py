5c9656f47d8a11194094665142a593e0c5c6203f
Grisha <grisha765@tuta.io>
2024-11-19 18:15:54 +0300

    [f] fix types

--- config/config.py
+++ config/config.py
@@ -6,15 +6,17 @@ class Config:
     tests: str = "False"
     tgbot_pass: str = "123"
     hh_login: str = "test@example.com"
-    hh_passw: str = "password123"
 
     @classmethod
     def load_from_env(cls):
-        for key, value in cls.__annotations__.items():
+        for key in cls.__annotations__:
             env_value = os.getenv(key.upper())
             if env_value is not None:
-                if isinstance(value, int):
+                current_value = getattr(cls, key)
+                if isinstance(current_value, int):
                     setattr(cls, key, int(env_value))
+                elif isinstance(current_value, list):
+                    setattr(cls, key, env_value.split(","))
                 else:
                     setattr(cls, key, env_value)
 
